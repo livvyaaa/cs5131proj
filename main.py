db760b7f7bc07aee3ddb60c7f74b95e073274979
olivia <violetyy.accounts@skiff.com>
2025-03-16 15:54:35 +0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+import config # create config.py, then put tokens in (e.g, X_API_TOKEN = "")
+
+import tensorflow as tf
+import numpy as np
+import pandas as pd
+import sklearn  # For scikit-learn
+import matplotlib.pyplot as plt
+import seaborn as sns
+import cv2  # OpenCV
+from PIL import Image  # Pillow
+from tqdm import tqdm
+import h5py
+import keras
+import tensorflow_datasets as tfds
+import tensorflow_hub as hub
+import tensorflow_probability as tfp
